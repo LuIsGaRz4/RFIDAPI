8c801abb130f648f919aadcd99b67c5e67ac3c12
Luis Garza <luis.garza@landisgyr.com>
2025-07-16 10:41:56 -0500

    cambios

--- RFID-API/Program.cs
+++ RFID-API/Program.cs
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-﻿using Microsoft.EntityFrameworkCore;
-using RFID_API.Data;
-using Microsoft.AspNetCore.SignalR;
-using RFID_API.Hubs; // Asegúrate de tener este using si defines un Hub
-=======
 ﻿using RFID_API.Data;
 using Microsoft.EntityFrameworkCore;
 using RFID_API.Hubs; // 👈 importa tu Hub
->>>>>>> b71045f101fd9673d0bdb68be404bbd4a2d684e7
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,27 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-<<<<<<< HEAD
-// 🔧 Controladores, Swagger y SignalR
-=======
 // Agregar servicios necesarios
->>>>>>> b71045f101fd9673d0bdb68be404bbd4a2d684e7
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR(); // ✅ Para SignalR
 
-<<<<<<< HEAD
-// 🔐 CORS para permitir acceso desde tu Angular Static Web App
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAngular", policy =>
-        policy
-            .WithOrigins("https://white-stone-0d90a691e.2.azurestaticapps.net")
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials()); // ✅ Permitir credenciales (cookies, auth, etc.)
-=======
 // ✅ Agrega SignalR
 builder.Services.AddSignalR();
 
@@ -48,7 +26,6 @@ builder.Services.AddCors(options =>
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials()); // 👈 necesario para SignalR con WebSockets
->>>>>>> b71045f101fd9673d0bdb68be404bbd4a2d684e7
 });
 
 var app = builder.Build();
@@ -64,19 +41,9 @@ app.UseCors("AllowAngular");
 
 app.UseAuthorization();
 
-<<<<<<< HEAD
-// 🧭 Mapear tus controladores
-app.MapControllers();
-
-// 🔔 Mapear el Hub de SignalR (esto es lo nuevo)
-app.MapHub<NotificationHub>("/hub/notificaciones");
-
-
-=======
 app.MapControllers();
 
 // ✅ Mapea el SignalR hub
 app.MapHub<NotificationHub>("/hub/notificaciones");
 
->>>>>>> b71045f101fd9673d0bdb68be404bbd4a2d684e7
 app.Run();
